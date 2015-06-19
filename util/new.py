14b8f7c830431fe8c94d306b87e1af2d8f2a67cd
Otavio Carneiro <otaviocsantos@gmail.com>
2015-06-18 22:03:27 -0300

    script new gravando em arquivo

--- util/new.py
+++ util/new.py
@@ -4,12 +4,6 @@ import datetime, sys
 #obtém a hora atual e a imprime
 now = datetime.datetime.now()
 (year,month,day,hour,minute)=(now.year, now.month, now.day, now.hour, now.minute)
-#year=now.year
-#month=now.month
-#day=now.day
-#hour=now.hour
-#minute=now.minute
-print("Date: {}-{:02}-{:02} {:02}:{:02}".format(year,month,day,hour,minute))
 
 #trata nome informado na linha de comando
 if len(sys.argv)>1:
@@ -19,10 +13,20 @@ else:
     title = input("Nome do artigo? ")
 print("Title: {}".format(title))
 
+print("Date: {}-{:02}-{:02} {:02}:{:02}".format(year,month,day,hour,minute))
+
 #cria o slug a partir do nome, trocando espaços por -
 slug = "{}-{:02}-{:02}-{}".format(year,month,day,title.replace(" ", "-"))
 print("Slug: {}".format(slug))
 
+
+
+with open(slug + ".md", "w") as post:
+    print("Title: {}".format(title), file=post)
+    print("Date: {}-{:02}-{:02} {:02}:{:02}".format(year,month,day,hour,minute), file=post)
+    print("Author: Otávio Carneiro", file=post)
+    print("Slug: {}".format(slug), file=post)
+    print("Tags: ", file=post)
 #Title: Minecraft para a Educação - Parte 1
 #Date: 2015-06-04 15:36
 #Author: Otávio Carneiro
