27d0eec82dc867117341313f36173c3384449347
Otavio Carneiro <otavio@carneiro.blog.br>
2015-10-25 10:45:56 -0200

    Script para criar novo post

---
+++ util/new.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+#Title: Minecraft para a Educação - Parte 1
+#Date: 2015-06-04 15:36
+#Author: Otávio Carneiro
+#Slug: Minecraft-para-a-Educacao-Parte-1
+#Tags: python, minecraft, cidadania, educação, raspberry pi
